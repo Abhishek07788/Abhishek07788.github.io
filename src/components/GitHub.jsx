8433a5a8a745e71a762f5fc3f990b66d4735fa62
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2022-09-19 00:34:32 +0530

    Github Calender Added

--- src/components/GitHub.jsx
+++ src/components/GitHub.jsx
@@ -20,6 +20,8 @@ const GitHub = ({ theme1 }) => {
         transition="all .8s ease-in-out"
         _hover={{
           transform: "scale(1.2)",
+          boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px'
+
         }}
       >
         <GitHubCalendar
