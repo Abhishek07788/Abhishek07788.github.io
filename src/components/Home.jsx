f100c501e5cb6725a5de92336d149bda5c2fa160
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2022-09-23 16:48:37 +0530

    A

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -47,24 +47,25 @@ const Home = (props) => {
         columns={[1, 1, 2, 2]}
         m="auto"
         alignItems={"center"}
-        mt={0.5}
+        mt={[-6, -5, 0.5, 0]}
         pt={["65px", "65px", "111px", "145px"]}
         pb={"105px"}
+        className="HomeMain"
         style={theme}
       >
         {/*======================== Left side Div ======================= */}
 
-        <SimpleGrid
+        <SimpleGrid className="HomeBox1" 
           m={["auto", "auto", "auto", "auto"]}
           w={["98%", "98%", "98%", "85%"]}
         >
           {/*======================== Hello heading ======================= */}
 
           <Heading
-            fontSize={[25, 25, 25, 30]}
+            fontSize={[20, 22, 25, 30]}
             color={"#1a202c"}
             fontFamily={"'Times New Roman', Times, serif"}
-            ml={[0, 0, 0, 50]}
+            ml={[-10, 13, 0, 50]}
             alignItems="center"
             display="flex"
             style={theme}
@@ -86,8 +87,8 @@ const Home = (props) => {
 
           <Heading
             mt={[0, 0, 0, 2]}
-            ml={[0, 0, 0, 50]}
-            fontSize={[32, 32, 32, 50]}
+            ml={[-10, 13, 0, 50]}
+            fontSize={[30, 30, 32, 50]}
           >
             Abhishek Pratap Solanki
           </Heading>
@@ -96,7 +97,8 @@ const Home = (props) => {
 
           <div className="tagLine">
             <Heading
-              ml={[0, 0, 0, 50]}
+              mt={[-4, -4, 0, 0]}
+              ml={[-10, 13, 0, 50]}
               fontSize={[23, 24, 23, 30]}
               id="madeByAbhishek"
             >
@@ -105,20 +107,111 @@ const Home = (props) => {
             </Heading>
           </div>
 
+          {/*======================== Social Media Icons ======================= */}
+          <Box
+            bottom="0"
+            right="0"
+            display={"flex"}
+            w={"60%"}
+            ml={[-10, 13, 0, 50]}
+            mt={5}
+            gap={2}
+            pb={["80px", "80px", "80px", "80px"]}
+          >
+            <a
+              href="http://www.linkedin.com/in/abhishekpratapsolanki"
+              target="_blank"
+            >
+              <Button
+                color="black"
+                border={"1px"}
+                bg="#ffb153"
+                fontSize={["16px", "17px", "20px", "20px"]}
+                p={[0, 0, 2, 2]}
+                borderRadius="50%"
+                transition="all .3s ease-in-out"
+                _hover={{
+                  border: "2px solid #0a66c2",
+                  transform: "scale(1.2)",
+                  bg: "#ffff",
+                  color: "#0a66c2",
+                }}
+              >
+                <AiFillLinkedin />
+              </Button>
+            </a>
+            <a href="https://github.com/Abhishek07788" target="_blank">
+              <Button
+                color="black"
+                border={"1px"}
+                bg="#ffb153"
+                fontSize={["16px", "17px", "20px", "20px"]}
+                p={[0, 0, 2, 2]}
+                borderRadius="50%"
+                transition="all .3s ease-in-out"
+                _hover={{
+                  transform: "scale(1.2)",
+                  border: "2px solid #3f3d55",
+                  bg: "#ffff",
+                  color: "#3f3d55",
+                }}
+              >
+                <BsGithub />
+              </Button>
+            </a>
+            <a href="mailto:abhisheksolanki1020@gmail.com">
+              <Button
+                color="black"
+                border={"1px"}
+                bg="#ffb153"
+                fontSize={["16px", "17px", "20px", "20px"]}
+                p={[0, 0, 2, 2]}
+                borderRadius="50%"
+                transition="all .3s ease-in-out"
+                _hover={{
+                  transform: "scale(1.2)",
+                  bg: "#ffff",
+                  border: "2px solid #f04244",
+                  color: "#f04244",
+                }}
+              >
+                <MdEmail />
+              </Button>
+            </a>
+            <a href="tel:+919758207006">
+              <Button
+                color="black"
+                border={"1px"}
+                bg="#ffb153"
+                borderRadius={"50%"}
+                fontSize={["16px", "17px", "20px", "20px"]}
+                p={[0, 0, 2, 2]}
+                transition="all .3s ease-in-out"
+                _hover={{
+                  transform: "scale(1.2)",
+                  bg: "#ffff",
+                  border: "2px solid #4db946",
+                  color: "#4db946",
+                }}
+              >
+                <BiPhoneCall />
+              </Button>
+            </a>
+          </Box>
+
           {/*======================== Resume buttons ======================= */}
 
-          <SimpleGrid display={"flex"}>
+          <SimpleGrid display={"flex"}  mt={["-16","-16","-65","-65"]}>
             {/*======================== View Resume ======================= */}
 
             <a href={resume}>
               <Button
-                border="2px solid #fe9119"
-                ml={[0, 0, 0, 50]}
-                mt={8}
-                bg={"#fe9119"}
+                border="2px solid #ffb153"
+                ml={[-10, 13, 0, 50]}
+                bg={"#ffb153"}
                 color="black"
-                fontSize={["14px", "14px", "18px"]}
-                w={["90px", "90px", "120px"]}
+                fontSize={["14px", "14px", "16x"]}
+                w={["90px", "90px", "100px"]}
                 mb={"40px"}
                 transition="all .3s ease-in-out"
                 _hover={{
@@ -144,10 +237,9 @@ const Home = (props) => {
                     isClosable: true,
                   })
                 }
-                border="2px solid #fe9119"
+                border="2px solid #ffb153"
                 ml={3}
-                mt={8}
-                bg={"#fe9119"}
+                bg={"#ffb153"}
                 color="black"
                 fontSize={["18px", "18px", "18px"]}
                 w={["30px", "30px", "40px"]}
@@ -163,106 +255,15 @@ const Home = (props) => {
               </Button>
             </a>
           </SimpleGrid>
-        
-            {/*======================== Social Media Icons ======================= */}      
-          <Box
-            bottom="0"
-            right="0"
-            display={"flex"}
-            w={"60%"}
-            ml={[0, 0, 0, 50]}
-            mt={-3}
-            gap={2}
-            pb={["80px", "80px", "80px", "80px"]}
-          >
-            <a
-              href="http://www.linkedin.com/in/abhishekpratapsolanki"
-              target="_blank"
-            >
-              <Button
-                color="#00000"
-                border={"1px"}
-                bg="#fe9119"
-                fontSize={["16px", "17px", "23px", "23px"]}
-                p={[0, 0, 2, 2]}
-                borderRadius="50%"
-                transition="all .3s ease-in-out"
-                _hover={{
-                  border: "2px solid #0a66c2",
-                  transform: "scale(1.2)",
-                  bg: "#ffff",
-                  color: "#0a66c2",
-                }}
-              >
-                <AiFillLinkedin />
-              </Button>
-            </a>
-            <a href="https://github.com/Abhishek07788" target="_blank">
-              <Button
-                color="#00000"
-                border={"1px"}
-                bg="#fe9119"
-                fontSize={["16px", "17px", "23px", "23px"]}
-                p={[0, 0, 2, 2]}
-                borderRadius="50%"
-                transition="all .3s ease-in-out"
-                _hover={{
-                  transform: "scale(1.2)",
-                  border: "2px solid #3f3d55",
-                  bg: "#ffff",
-                  color: "#3f3d55",
-                }}
-              >
-                <BsGithub />
-              </Button>
-            </a>
-            <a href="mailto:abhisheksolanki1020@gmail.com">
-              <Button
-                color="#00000"
-                border={"1px"}
-                bg="#fe9119"
-                fontSize={["16px", "17px", "23px", "23px"]}
-                p={[0, 0, 2, 2]}
-                borderRadius="50%"
-                transition="all .3s ease-in-out"
-                _hover={{
-                  transform: "scale(1.2)",
-                  bg: "#ffff",
-                  border: "2px solid #f04244",
-                  color: "#f04244",
-                }}
-              >
-                <MdEmail />
-              </Button>
-            </a>
-            <a href="tel:+919758207006">
-              <Button
-                color="#00000"
-                border={"1px"}
-                bg="#fe9119"
-                borderRadius={"50%"}
-                fontSize={["16px", "17px", "23px", "23px"]}
-                p={[0, 0, 2, 2]}
-                transition="all .3s ease-in-out"
-                _hover={{
-                  transform: "scale(1.2)",
-                  bg: "#ffff",
-                  border: "2px solid #4db946",
-                  color: "#4db946",
-                }}
-              >
-                <BiPhoneCall />
-              </Button>
-            </a>
-          </Box>
         </SimpleGrid>
 
         {/*======================== My Image (Avatar) ======================= */}
 
-        <Box ml={["-26%", "-18%", "1%"]}>
+        <Box className="HomeBox2" ml={["-25%", "-18%", "1%"]}> 
           <Image
             borderRadius={"50%"}
             border={"1px solid #dc3544"}
+            mt="-3"
             m={"auto"}
             w={[320, 320, 290, 380]}
             transition="all 1s ease-in-out"
